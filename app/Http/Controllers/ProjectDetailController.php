0fb3ee95ac32baa0b59730cbf0128a143c713ca9
Avinash <avinash.glocify@gmail.com>
2020-02-17 17:38:14 +0530

    file validation added

--- app/Http/Controllers/ProjectDetailController.php
+++ app/Http/Controllers/ProjectDetailController.php
@@ -10,6 +10,8 @@ use App\Imports\ScreaminFrogCrawlsImport;
 use App\Imports\SemRushImport;
 use App\Imports\AhrefsImport;
 use Maatwebsite\Excel\Facades\Excel;
+use Illuminate\Support\Facades\Response;
+use Illuminate\Support\Facades\Validator;
 
 use App\Models\Project;
 
@@ -17,8 +19,20 @@ class ProjectDetailController extends Controller
 {
     public function storeSitemap(Request $request, $id)
     {
-        $rules   = ['sitemap_file' => 'required'];
-        $request->validate($rules);
+        $validator = Validator::make($request->all(), [
+            'sitemap_file' => ['required',
+                function ($attribute, $value, $fail) {
+                  $extension = $value->getClientOriginalExtension();
+                    if(!in_array($extension, ['csv', 'xslx'])) {
+                      $fail('File must be type of csv or xslx');
+                    }
+                },
+              ]
+        ]);
+
+        if($validator->fails()) {
+          return redirect()->back()->withErrors($validator->errors())->withInput();
+        }
 
         $project = Project::findOrFail($id);
         $import  = new SitemapImport($project);
@@ -29,8 +43,21 @@ class ProjectDetailController extends Controller
 
     public function storeConsole(Request $request, $id)
     {
-        $rules   = ['console_file' => 'required'];
-        $request->validate($rules);
+
+        $validator = Validator::make($request->all(), [
+            'console_file' => ['required',
+                function ($attribute, $value, $fail) {
+                  $extension = $value->getClientOriginalExtension();
+                    if(!in_array($extension, ['csv', 'xslx'])) {
+                      $fail('File must be type of csv or xslx');
+                    }
+                },
+              ]
+        ]);
+
+        if($validator->fails()) {
+          return redirect()->back()->withErrors($validator->errors())->withInput();
+        }
 
         $project = Project::findOrFail($id);
         $import  = new GoogleSearchConsoleImport($project);
@@ -41,37 +68,76 @@ class ProjectDetailController extends Controller
 
     public function storeAhrefs(Request $request, $id)
     {
-        $rules   = ['ahrefs_file' => 'required'];
-        $request->validate($rules);
+
+        $validator = Validator::make($request->all(), [
+            'ahrefs_file' => ['required',
+                function ($attribute, $value, $fail) {
+                  $extension = $value->getClientOriginalExtension();
+                    if(!in_array($extension, ['csv', 'xslx'])) {
+                      $fail('File must be type of csv or xslx');
+                    }
+                },
+              ]
+        ]);
+
+        if($validator->fails()) {
+          return redirect()->back()->withErrors($validator->errors())->withInput();
+        }
 
         $project = Project::findOrFail($id);
         $import  = new AhrefsImport($project);
 
         Excel::import($import,  request()->file('ahrefs_file'));
-        return redirect()->back()->with(['success' => 'Main Keywords File Imported SuccessFully' ]);
+        return redirect()->back()->with(['success' => 'Ahrefs File Imported SuccessFully' ]);
     }
 
     public function storeScreamingFrogs(Request $request, $id)
     {
-        $rules   = ['screaming_frogs_file' => 'required'];
-        $request->validate($rules);
+
+        $validator = Validator::make($request->all(), [
+            'screaming_frogs_file' => ['required',
+                function ($attribute, $value, $fail) {
+                  $extension = $value->getClientOriginalExtension();
+                    if(!in_array($extension, ['csv', 'xslx'])) {
+                      $fail('File must be type of csv or xslx');
+                    }
+                },
+              ]
+        ]);
+
+        if($validator->fails()) {
+          return redirect()->back()->withErrors($validator->errors())->withInput();
+        }
 
         $project = Project::findOrFail($id);
         $import  = new ScreaminFrogCrawlsImport($project);
 
         Excel::import($import,  request()->file('screaming_frogs_file'));
-        return redirect()->back()->with(['success' => 'Main Keywords File Imported SuccessFully' ]);
+        return redirect()->back()->with(['success' => 'Screaming Frogs File Imported SuccessFully' ]);
     }
 
     public function storeSemRush(Request $request, $id)
     {
-        $rules   = ['sem_rush_file' => 'required'];
-        $request->validate($rules);
+
+        $validator = Validator::make($request->all(), [
+            'sem_rush_file' => ['required',
+                function ($attribute, $value, $fail) {
+                  $extension = $value->getClientOriginalExtension();
+                    if(!in_array($extension, ['csv', 'xslx'])) {
+                      $fail('File must be type of csv or xslx');
+                    }
+                },
+              ]
+        ]);
+
+        if($validator->fails()) {
+          return redirect()->back()->withErrors($validator->errors())->withInput();
+        }
 
         $project = Project::findOrFail($id);
         $import  = new SemRushImport($project);
 
         Excel::import($import,  request()->file('sem_rush_file'));
-        return redirect()->back()->with(['success' => 'Main Keywords File Imported SuccessFully' ]);
+        return redirect()->back()->with(['success' => 'SEM Rush File Imported SuccessFully' ]);
     }
 }
