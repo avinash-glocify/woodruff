dbcac26446f02cb5de75a8814b3e29963aafb1ea
Avinash <avinash.glocify@gmail.com>
2020-02-18 12:00:22 +0530

    routes updated

--- routes/web.php
+++ routes/web.php
@@ -5,28 +5,34 @@ Auth::routes();
 Route::group(['middleware' => 'auth'], function() {
   Route::get('/', 'HomeController@index')->name('home');
   Route::resource('project', 'ProjectController');
-  // import routes
-  Route::post('project/sitemap/{id}', 'ProjectDetailController@storeSitemap')->name('project.sitemap');
-  Route::post('project/console/{id}', 'ProjectDetailController@storeConsole')->name('project.console');
-  Route::post('project/ahrefs/{id}', 'ProjectDetailController@storeAhrefs')->name('project.Ahrefs');
-  Route::post('project/screaming_frogs/{id}', 'ProjectDetailController@storeScreamingFrogs')->name('project.screaming_frogs');
-  Route::post('project/sem_rush/{id}', 'ProjectDetailController@storeSemRush')->name('project.sem_rush');
+
+  Route::group(['prefix' => 'project'], function() {
+    // import routes
+    Route::post('sitemap/{id}', 'ProjectDetailController@storeSitemap')->name('project.sitemap');
+    Route::post('console/{id}', 'ProjectDetailController@storeConsole')->name('project.console');
+    Route::post('ahrefs/{id}', 'ProjectDetailController@storeAhrefs')->name('project.Ahrefs');
+    Route::post('screaming_frogs/{id}', 'ProjectDetailController@storeScreamingFrogs')->name('project.screaming_frogs');
+    Route::post('sem_rush/{id}', 'ProjectDetailController@storeSemRush')->name('project.sem_rush');
+
+    Route::get('{id}/sitemap', 'ProjectController@sitemap')->name('sitemap');
+    Route::get('{id}/setup', 'ProjectController@show')->name('setup');
+    Route::get('{id}/google-search-console', 'ProjectController@googleConsole')->name('google-search-console');
+    Route::get('{id}/ahrefs', 'ProjectController@aHrefs')->name('ahrefs');
+    Route::get('{id}/aggregation', 'ProjectController@aggregation')->name('aggregation');
+    Route::get('{id}/screaming-frogs', 'ProjectController@screamingFrogs')->name('screaming_frogs');
+    Route::get('{id}/sem-rush', 'ProjectController@semRush')->name('sem-rush');
+
+    //data table routes
+    Route::get('{id}/aggregation/data', 'ProjectController@aggregationData')->name('aggregationData');
+    Route::get('{id}/sitemap/data', 'ProjectController@sitemapData')->name('sitemapData');
+    Route::get('{id}/google-search-console/data', 'ProjectController@searchConsoleData')->name('searchConsoleData');
+    Route::get('{id}/ahrefs/data', 'ProjectController@ahrefsData')->name('ahrefsData');
+    Route::get('{id}/screaming-frogs/data', 'ProjectController@screamingFrogData')->name('screamingFrogData');
+    Route::get('{id}/sem-rush/data', 'ProjectController@semRushData')->name('semRushData');
+  });
 
   Route::get('sample/{type}', 'ProjectController@downloadSample')->name('project.sample');
 
-  Route::get('project/{id}/sitemap', 'ProjectController@sitemap')->name('sitemap');
-  Route::get('project/{id}/setup', 'ProjectController@show')->name('setup');
-  Route::get('project/{id}/google-search-console', 'ProjectController@googleConsole')->name('google-search-console');
-  Route::get('project/{id}/ahrefs', 'ProjectController@aHrefs')->name('ahrefs');
-  Route::get('project/{id}/aggregation', 'ProjectController@aggregation')->name('aggregation');
-  Route::get('project/{id}/screaming-frogs', 'ProjectController@screamingFrogs')->name('screaming_frogs');
-  Route::get('project/{id}/sem-rush', 'ProjectController@semRush')->name('sem-rush');
-  Route::get('project/{id}/aggregation/data', 'ProjectController@aggregationData')->name('aggregationData');
-  Route::get('project/{id}/sitemap/data', 'ProjectController@sitemapData')->name('sitemapData');
-  Route::get('project/{id}/google-search-console/data', 'ProjectController@searchConsoleData')->name('searchConsoleData');
-  Route::get('project/{id}/ahrefs/data', 'ProjectController@ahrefsData')->name('ahrefsData');
-  Route::get('project/{id}/screaming-frogs/data', 'ProjectController@screamingFrogData')->name('screamingFrogData');
-  Route::get('project/{id}/sem-rush/data', 'ProjectController@semRushData')->name('semRushData');
   // users routes
   Route::resource('user', 'UsersController');
 });
